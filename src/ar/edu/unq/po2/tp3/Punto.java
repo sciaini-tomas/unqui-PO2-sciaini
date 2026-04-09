55dfa5ee477023d7e089d41c5f0a8c5e6270a3fd
Sciaini, Tomas <141785231+sciaini-tomas@users.noreply.github.com>
2026-04-09 18:21:41 -0300

    version .1

---
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -0,0 +1,33 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto {
+	private float x;
+	private float y;
+	
+	public Punto(float x,float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	// Getter y Setter X
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	//////////////////////////
+	
+	//Getter y setter Y
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	////////////////////////////
+}
